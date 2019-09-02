89e98e8796472240412c4a837ac42a149ee9f581
amiralichesser3 <Safari_1234>
2019-09-03 00:18:54 +0430

    added viewpager to scenarioActivity

--- app/src/main/java/com/safari/drfoot/fragments/GameFragment1.kt
+++ app/src/main/java/com/safari/drfoot/fragments/GameFragment1.kt
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.safari.drfoot.R
 import kotlinx.android.synthetic.main.fragment_game_fragment2.*
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,7 +44,19 @@ class GameFragment1 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dummyText.text = param1.toString()
+        val adapter = FragmentPagerItemAdapter(
+            childFragmentManager, FragmentPagerItems.with(context)
+                .add("Demographics", DemographicsFragment::class.java)
+                .add("Presenting Complain", ComplainFragment::class.java)
+                .add("History", HistoryFragment::class.java)
+                .add("Examination", ExaminationFragment::class.java)
+                .add("Investigation", InvestigationFragment::class.java)
+                .add("Next Stage", NextStageFragment::class.java)
+                .create()
+        )
+
+        viewpager.adapter = adapter
+        viewpagertab.setViewPager(viewpager)
     }
 
     companion object {
